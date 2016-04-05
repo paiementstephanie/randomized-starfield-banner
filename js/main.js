ab84a0a8db98077a7373431fe0c57520002f9c0a
paiementstephanie <stephanie.paiement@rogers.com>
2016-04-05 17:28:22 -0400

    aaded newstar randomizing

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ var i = 0;
 var totalStars = 75;
 var $newStar;
 
+for (i = 0; i <totalStars; i++) {
+  $newStar = $('<div>');
+  $newStar.addClass('star');
+
+  $newStar.css({
+    top: Math.random() * bannerHeight,
+    left: Math.random() * bannerWidth,
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random()),
+    opacity: Math.random()
+  });
+
+  $banner.append($newStar);
